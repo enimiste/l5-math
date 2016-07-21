ef5615ae74896376f0647487b3da07972e599e7b
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-07-21 15:27:23 +0100

    Add new helpers functions

--- src/Helpers.php
+++ src/Helpers.php
@@ -9,13 +9,34 @@ if ( ! function_exists( 'calculator' ) ) {
 	}
 }
 
-if ( ! function_exists( 'isInteger' ) ) {
+if ( ! function_exists( 'check_is_integer' ) ) {
 	/**
 	 * @param string|int|float $value
 	 *
 	 * @return bool
 	 */
-	function isInteger( $value ) {
+	function check_is_integer( $value ) {
 		return \Enimiste\Math\VO\IntegerNumber::isInt( $value );
+	}
+}
+
+if ( ! function_exists( 'as_float_number' ) ) {
+	/**
+	 * @param string|int|float|\Enimiste\Math\VO\FloatNumber $value
+	 *
+	 * @param int                                            $scale
+	 *
+	 * @return bool
+	 */
+	function as_float_number( $value, $scale = 2 ) {
+		if ( $value instanceof \Enimiste\Math\VO\FloatNumber ) {
+			return $value->copy( $scale );
+		} else {
+			if ( $scale < 0 ) {
+				throw new \RuntimeException( 'Scale should be >= 0' );
+			}
+
+			return new \Enimiste\Math\VO\FloatNumber( $value, $scale );
+		}
 	}
 }
